1ddde751fee46adfcdfcfea29f23fa5730229222
SavindaJayasekara <thantrige32@gmail.com>
2023-10-23 18:27:37 +0530

    add text function

--- assets/js/index.js
+++ assets/js/index.js
@@ -3,3 +3,8 @@ let secondNum;
 let opt;
 let display = $('#display-text');
 let oparator = $('#oparator');
+
+$('#button-section .num').click(function () {
+    let text = $(this).children().text();
+    display.text(display.text() + text);
+});
